7f1c9b422f124b29ea16380ec5c502278a828358
Salvatore Pitanza <pitanza.salvatore.86@gmail.com>
2022-04-27 17:28:36 +0200

    added messages table

---
+++ database/migrations/2022_04_27_152318_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('apartment_id')->constrained();
+            $table->string('email')->required();
+            $table->string('content')->required();
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
