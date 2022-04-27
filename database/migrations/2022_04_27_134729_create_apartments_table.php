3733a840f6089152ee2d7d9f0b9a8cce97f50d27
Vasmayer <lealimarco90@gmail.com>
2022-04-27 15:53:45 +0200

    first commit

---
+++ database/migrations/2022_04_27_134729_create_apartments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('title_desc')->required();
+            $table->string('image')->nullable();
+            $table->string('room')->required();
+            $table->string('bathroom')->required();
+            $table->string('bed')->required();
+            $table->bigInteger('square_meters')->required();
+            $table->boolean('visible')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
