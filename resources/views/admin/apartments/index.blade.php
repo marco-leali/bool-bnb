42db05e9ab13750afabfb3bb49c8dfe62abed992
Gioelinz <gioelerunza.gr@gmail.com>
2022-04-28 18:28:07 +0200

    add count of views and better show

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -11,29 +11,36 @@
                     <th scope="col">Letti</th>
                     <th scope="col">MQ</th>
                     <th scope="col">Visibile</th>
+                    <th scope="col">Visualizzazioni</th>
                     <th scope="col">Azioni</th>
                 </tr>
             </thead>
             <tbody>
                 @forelse ($apartments as $apartment)
-                    <tr>
-                        <td><img src="{{ $apartment->image }}" alt="immagine appartamento" width='50' height='50'></td>
+                    <tr class="align-middle">
+                        <td>
+                            <a href="{{ route('admin.apartments.show', $apartment->id) }}">
+                                <img class="img-scale" src="{{ $apartment->image }}" alt="immagine appartamento"
+                                    width='50' height='50'>
+                            </a>
+                        </td>
                         <td>{{ Str::limit($apartment->title_desc, 10) }}</td>
                         <td>{{ $apartment->room }}</td>
                         <td>{{ $apartment->bathroom }}</td>
                         <td>{{ $apartment->bed }}</td>
                         <td>{{ $apartment->square_meters }}</td>
                         <td> {!! $apartment->visible ? '<i class="fa-solid fa-eye fa-lg"></i>' : '<i class="fa-solid fa-eye-slash fa-lg"></i>' !!}</td>
+                        <td>{{ count($apartment->ipaddresses) }}</td>
                         <td>
                             <a href="{{ route('admin.apartments.show', $apartment->id) }}"
                                 class="btn btn-small btn-primary rounded-circle text-light"><i
                                     class="fa-solid fa-magnifying-glass"></i></a>
                             <a href="{{ route('admin.apartments.edit', $apartment->id) }}"
-                                class="btn btn-small btn-warning rounded-circle"><i
-                                    class="fa-solid fa-pencil"></i></a>
-                                    <form class="d-inline" action="">
-                                        <button class="btn btn-small btn-danger rounded-circle text-light"><i class="fa-solid fa-trash "></i></button>
-                                    </form>
+                                class="btn btn-small btn-warning rounded-circle"><i class="fa-solid fa-pencil"></i></a>
+                            <form class="d-inline" action="">
+                                <button class="btn btn-small btn-danger rounded-circle text-light"><i
+                                        class="fa-solid fa-trash "></i></button>
+                            </form>
                         </td>
                     @empty <td class="text-center fs-1" colspan="7">Non hai appartamenti registrati</td>
                     </tr>
--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -3,14 +3,21 @@
 @section('content')
     <div class="container">
         <div class="card">
-            <img src="{{ $apartment->image }}" class="card-img-top" alt="Immagine Appartamento">
-            <div class="card-body">
-                <h5 class="card-title">Appartamento: {{ $apartment->title_desc }}</h5>
-                <p class="card-text">Stanze: {{ $apartment->room }}</p>
-                <p class="card-text">Bagni: {{ $apartment->bathroom }}</p>
-                <p class="card-text">Letti: {{ $apartment->bed }}</p>
-                <p class="card-text">Metri quadrati: {{ $apartment->square_meters }}</p>
-                <a href="{{ route('admin.apartments.index') }}" class="btn btn-primary text-light">Torna indietro</a>
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="{{ $apartment->image }}" class="card-img-top" alt="Immagine Appartamento">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Appartamento: {{ $apartment->title_desc }}</h5>
+                        <p class="card-text">Stanze: {{ $apartment->room }}</p>
+                        <p class="card-text">Bagni: {{ $apartment->bathroom }}</p>
+                        <p class="card-text">Letti: {{ $apartment->bed }}</p>
+                        <p class="card-text">Metri quadrati: {{ $apartment->square_meters }}</p>
+                        <a href="{{ route('admin.apartments.index') }}" class="btn btn-primary text-light">Torna
+                            indietro</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
