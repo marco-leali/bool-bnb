747f283330d343c811156b1cdd872aa76dc3f261
Narresx <antonionicodemonick@gmail.com>
2022-05-09 12:36:57 +0200

    add pack detail page

---
+++ resources/js/components/Pages/Apartments/PacksDetail.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="container text-center fs-1 mb-3">
+    <div>In basso troverai le nostre migliori offerte per sponsorizzare il tuo appartamento</div>
+<div class="row g-5">
+    <div class="text-center col-sm-12 col-md-6 col-lg-4 col-xl-4">
+        <div><i class="fa-solid fa-medal fa-lg fs-1 text-danger"></i></div>
+        <h2>Pacchetto Bronze</h2>
+        <div class="fs-3">Sponsorizza un appartamento per 24 ore</div>
+        <div><span><i class="fa-solid fa-euro-sign"></i></span>2,99 </div>
+    </div>
+    <div class="text-center col-sm-12 col-md-6 col-lg-4 col-xl-4">
+        <div><i class="fa-solid fa-medal fa-lg fs-1 text-muted"></i></div>
+        <h2>Pacchetto Silver</h2>
+        <div class="fs-3">Sponsorizza un appartamento per 72 ore</div>
+        <div><span><i class="fa-solid fa-euro-sign"></i></span>5,99 </div>
+    </div>
+    <div class="text-center col-sm-12 col-md-6 col-lg-4 col-xl-4">
+        <div><i class="fa-solid fa-medal fa-lg fs-1 text-warning"></i></div>
+        <h2>Pacchetto Gold</h2>
+        <div class="fs-3">Sponsorizza un appartamento per 144 ore</div>
+        <div><span><i class="fa-solid fa-euro-sign"></i></span>9,99 </div>
+    </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+name:"PacksDetail"
+}
+</script>
+
+<style>
+
+</style>
